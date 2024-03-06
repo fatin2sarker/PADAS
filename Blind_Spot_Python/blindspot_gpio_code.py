a53119cee207b42a4fe3233ba77268bd3695153e
Jessica <jmbertle@uwaterloo.ca>
2024-03-06 01:11:22 -0500

    updated code

--- Blind_Spot_Python/blindspot_gpio_code.py
+++ Blind_Spot_Python/blindspot_gpio_code.py
@@ -3,34 +3,34 @@ import time
 
 def main():
     # Set up GPIO using BCM numbering
-GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BCM)
 
 # Set the pin number you want to monitor
-pin = 17
+    pin = 17
 
 # Set the pin as input
-GPIO.setup(pin, GPIO.IN)
+    GPIO.setup(pin, GPIO.IN)
 
 # Initial state of the pin
-prev_state = GPIO.input(pin)
+    prev_state = GPIO.input(pin)
 
-try:
-    while True:
-        # Read the current state of the pin
-        current_state = GPIO.input(pin)
+    try:
+        while True:
+            # Read the current state of the pin
+            current_state = GPIO.input(pin)
         
-        # Check if the state has changed
-        if current_state != prev_state:
-            print("check blindspot!")
+            # Check if the state has changed
+            if current_state != prev_state:
+                print("check blindspot!")
 
-        # Update the previous state
-        prev_state = current_state
+            # Update the previous state
+            #prev_state = current_state
         
-        # Delay for a short time to avoid busy waiting
-        time.sleep(0.1)
+            # Delay for a short time to avoid busy waiting
+            #time.sleep(0.1)
 
-except KeyboardInterrupt:
-    GPIO.cleanup()  # Clean up GPIO on exit
+    except KeyboardInterrupt:
+        GPIO.cleanup()  # Clean up GPIO on exit
 
 if __name__ == "__main__":
     main()
